40284ca749363fda8ace854901cd2e82d4d1d64f
Minichiellon <92965517+Minichiellon@users.noreply.github.com>
2024-03-21 17:23:39 +0800

    Update main.c

--- 数据结构/5.树/树的c语言实现/main.c
+++ 数据结构/5.树/树的c语言实现/main.c
@@ -1,6 +1,14 @@
-#include"tree.h"
+#include "tree.h"
+#include <stdio.h>
 int main(void) {
-    BiTree tr = CreateBiTree();
+    //BiTree tr[7];
+    BiTree tr;
+    tr = CreateBiTree();
+
+    PreOrderTraverse(tr);
+    printf("\n");
     InOrderTraverse(tr);
+    printf("\n");
+    PostOrderTraverse(tr);
     return 0;
 }
