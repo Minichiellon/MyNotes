91ebad2404fe3c9f5cfc519b2671085f4401b08a
Minichiellon <92965517+Minichiellon@users.noreply.github.com>
2024-03-22 16:35:27 +0800

    Create main.c

---
+++ 数据结构/3.栈和队列/队列的c语言实现/main.c
@@ -0,0 +1,43 @@
+#include "queue.h"
+#include <stdio.h>
+int main(void){
+    char mem[10];
+    
+    SqQueue que;
+    SqQueue* qu = &que;
+    InitQueue(qu);
+
+    EnQueue(qu, 'a');
+    printQueue(qu);
+    printf("\n");
+
+    EnQueue(qu, 'b');
+    printQueue(qu);
+    printf("\n");
+
+    EnQueue(qu, 'c');
+    printQueue(qu);
+    printf("\n");
+
+    EnQueue(qu, 'd');
+    printQueue(qu);
+    printf("\n");
+
+    ClearQueue(qu);
+    printQueue(qu);
+    printf("\n");
+
+    DeQueue(qu,&mem[0]);
+    printQueue(qu);
+    printf("\n");
+
+    DeQueue(qu,&mem[1]);
+    printQueue(qu);
+    printf("\n");
+
+    EnQueue(qu, 'f');
+    printQueue(qu);
+    printf("\n");
+    
+    return 0;
+}
