a41694b9b8955aebbd8b68cdb0fbf843687d2ff9
Minichiellon <92965517+Minichiellon@users.noreply.github.com>
2024-03-23 02:22:04 +0800

    Update queue.h

--- 数据结构/3.栈和队列/队列的c语言实现/queue.h
+++ 数据结构/3.栈和队列/队列的c语言实现/queue.h
@@ -1,7 +1,6 @@
 #ifndef QUEUE_H
 #define QUEUE_H
-
-#define MAXQSIZE 50
+#define MAXQSIZE 10
 #define OK 1
 #define ERROR 0
 #define CONST_0 0
@@ -13,13 +12,13 @@ typedef struct {
     int rear;        //尾指针
     int QueueSize;   //队列大小
 }SqQueue;
-bool InitQueue(SqQueue*);
-bool EnQueue(SqQueue *Q, QElemType e);
-bool DeQueue(SqQueue *Q);
-int QueueLength(const SqQueue* Q);
-QElemType GetHead(const SqQueue* Q);
-bool IsQueueEmpty(const SqQueue* Q);
-bool IsQueueFull(const SqQueue* Q);
-bool ClearQueue(SqQueue* qu);
-void printQueue(const SqQueue* Q);
+bool InitQueue(SqQueue*);               //初始化队列
+bool EnQueue(SqQueue *Q, QElemType e);  //入队
+bool DeQueue(SqQueue *Q);               //出队
+int QueueLength(const SqQueue* Q);      //求队列长度
+QElemType GetHead(const SqQueue* Q);    //获取队头元素
+bool IsQueueEmpty(const SqQueue* Q);    //队列是否为空
+bool IsQueueFull(const SqQueue* Q);     //队列是否已满
+bool ClearQueue(SqQueue* qu);           //清空队列
+void printQueue(const SqQueue* Q);      //打印队列
 #endif
