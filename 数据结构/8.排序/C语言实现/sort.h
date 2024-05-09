13945c77c82abb94598095baedac8ec837b22061
Minichiellon <92965517+Minichiellon@users.noreply.github.com>
2024-05-09 17:15:43 +0800

    Create sort.h

---
+++ 数据结构/8.排序/C语言实现/sort.h
@@ -0,0 +1,35 @@
+#ifndef __SORT_H
+#define __SORT_H
+
+#define MAXSIZE 100000
+
+typedef int DataType;
+typedef void (*p_SortFun)();
+typedef struct{
+    DataType data[MAXSIZE + 1];
+    int size;
+}array;
+
+void InsertSort(array *ar);     //直接插入排序
+void BinarySort(array *ar);     //折半插入排序
+void ShellSort(array *ar);      //希尔插入排序
+void ShellInsert(array *ar, int dk);
+
+void BubbleSort(array *ar);     //冒泡排序
+void QuickSort(array *ar, int low, int high);     //快速排序
+int Partition(array *ar, int low, int high);
+
+void SelectSort(array *ar);     //选择排序
+void HeapSort(array *ar);       //堆排序
+void HeapAdjust(array *ar, int NodeIndex, int size);
+
+void MergeSort(array* ar);      //归并排序
+int min(int x, int y);
+void BaseSort(array *ar);       //基数排序
+
+
+void printArray(array *ar);
+void GenerateArray(array *ar);
+void swap(DataType* a, DataType* b);
+
+#endif
