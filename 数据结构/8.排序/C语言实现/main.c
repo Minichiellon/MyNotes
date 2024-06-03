3e8f95915419fcc64d38ca2d593fa7b05ba5e8e5
Minichiellon <92965517+Minichiellon@users.noreply.github.com>
2024-06-03 14:58:57 +0800

    Update main.c

--- 数据结构/8.排序/C语言实现/main.c
+++ 数据结构/8.排序/C语言实现/main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include "sort.h"
 #include <time.h>
-
+#include <string.h>
 const p_SortFun p_fun[] = {InsertSort,BinarySort,ShellSort,BubbleSort,
-                            SelectSort,HeapSort,MergeSort,BaseSort};
+                            SelectSort,HeapSort,BaseSort,BaseSort};
 const char* SortMethod[] ={"InsertSort","BinarySort","ShellSort","BubbleSort",
                             "SelectSort","HeapSort","MergeSort","BaseSort","QuickSort"};
+const char ShowArray[] = "BaseSort";
 
 int main(void)
 {
     clock_t t[9];
     array ar;
-
-    for(int i = 0 ; i < 8 ; ++i)
+    for(int i = 0 ; i < sizeof(p_fun)/sizeof(p_fun[0]) ; ++i)
     {
         GenerateArray(&ar);
-        if(i == 7) {printArray(&ar);}
+        if(strcmp(SortMethod[i], ShowArray) == 0) {printArray(&ar);}
         t[i] = clock();
         p_fun[i](&ar);
         t[i] = clock() - t[i];
-        if(i == 7) {printArray(&ar);}
+        if(strcmp(SortMethod[i], ShowArray) == 0) {printArray(&ar);}
     }
     
     GenerateArray(&ar);
@@ -27,7 +27,7 @@ int main(void)
     QuickSort(&ar, 1, ar.size);
     t[8] = clock() - t[8];
 
-    for(int i = 0 ; i < 9 ; ++i)
+    for(int i = 0 ; i < sizeof(SortMethod)/sizeof(SortMethod[0]) ; ++i)
     {
         printf("%s:%f seconds\n",SortMethod[i],((float)t[i])/CLOCKS_PER_SEC);
     }
